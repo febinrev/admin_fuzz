b2d80bf7bb6da5c743283f18c1c28da0202f86a4
febinrev <52229330+febinrev@users.noreply.github.com>
2020-02-21 16:54:39 +0530

    Added SERVER Detection And Advanced error handling

--- FR-admin_fuzz.py
+++ FR-admin_fuzz.py
@@ -31,6 +31,8 @@ STARTED CRAWLING TO FIND ADMIN PANEL OF URL : \033[1;34m{url}
 			url=f"{url}/"
 			server=requests.get(url).headers.get('Server')
 			print(f"\033[1;37mSERVER Type >> {server}")
+			print("\n<----------------------------------------------------------------------------------->")
+			print(" ")
 		for i in range(len(adminpages)):
 			reqresp=requests.get(f"{url}/{adminpages[i]}",timeout=10)
 			if reqresp.status_code == 200:
